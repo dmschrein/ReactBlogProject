1b3b5ebe89d4ac3cb149bf6b85ad5e18a4418874
dmschrein <darrelmschreiner@gmail.com>
2024-04-09 11:59:58 -0700

    added blog pages and pagination

--- react-blog-website/src/App.jsx
+++ react-blog-website/src/App.jsx
@@ -1,13 +1,16 @@
 
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import './App.css'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-      <div>Navbar</div>
+     <Navbar/>
       <Outlet/>
+      <Footer/>
     </>
   )
 }
